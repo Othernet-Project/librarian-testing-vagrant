0bdfd90647042e0c2f2876ccd5b8e3b8e5dcf96c
Branko Vukelic <branko@brankovukelic.com>
2015-04-21 14:55:33 +0200

    Better layout for startup message

--- start.cmd
+++ start.cmd
@@ -1,14 +1,14 @@
 @echo off
 vagrant up
-echo =============================================================
-echo Librarian is now available at one of these addresses:
-echo
-echo     http://localhost:8080/
-echo     http://127.0.0.1:8080/
-echo
-echo           PRESS ANY KEY TO STOP THE VIRTUAL MACHINE
-echo 
-echo =============================================================
+echo "============================================================="
+echo "Librarian is now available at one of these addresses:        "
+echo "                                                             "
+echo "    http://localhost:8080/                                   "
+echo "    http://127.0.0.1:8080/                                   "
+echo "                                                             "
+echo "          PRESS ANY KEY TO STOP THE VIRTUAL MACHINE          "
+echo "                                                             "
+echo "============================================================="
 pause > null
 echo
 vagrant halt
