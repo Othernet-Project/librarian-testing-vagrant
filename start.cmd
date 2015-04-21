0fe55ba0872652c4192040cb2fa4b17a35594721
Branko Vukelic <branko@brankovukelic.com>
2015-04-21 14:21:57 +0200

    Print Librarian URI when starting

--- start.cmd
+++ start.cmd
@@ -1,6 +1,14 @@
 @echo off
 vagrant up
-echo Press any key to stop the virtual machine...
+echo =============================================================
+echo Librarian is now available at one of these addresses:
+echo
+echo     http://localhost:8080/
+echo     http://127.0.0.1:8080/
+echo
+echo           PRESS ANY KEY TO STOP THE VIRTUAL MACHINE
+echo 
+echo =============================================================
 pause > null
 echo
 vagrant halt
--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 cd "$(dirname "$0")"
 vagrant up
-read -n1 -p "Press any key to stop the virtual machine..."
+echo =============================================================
+echo Librarian is now available at one of these addresses:
+echo
+echo     http://localhost:8080/
+echo     http://127.0.0.1:8080/
+echo
+echo           PRESS ANY KEY TO STOP THE VIRTUAL MACHINE
+echo 
+echo =============================================================
+read -n1
 echo
 vagrant halt
